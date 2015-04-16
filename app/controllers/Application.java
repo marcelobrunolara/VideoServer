643db6e0e69e1e0d6c57ea7ab139096fe4479920
Marcelo Bruno Costa Lara <marcelobrunolara@gmail.com>
2015-04-16 00:45:07 -0300

    Criação da Model, Controller e View do projeto. Funcionalidade básica de exportação e formulário.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,37 @@ import play.mvc.*;
 
 import views.html.*;
 
+import models.S3File;
+import play.db.ebean.Model;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Http;
+
+import views.html.*;
+
+import java.util.List;
+import java.util.UUID;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<S3File> uploads = new Model.Finder(UUID.class, S3File.class).all();
+        return ok(index.render(uploads));
+    }
+
+    public static Result upload() {
+        Http.MultipartFormData body = request().body().asMultipartFormData();
+        Http.MultipartFormData.FilePart uploadFilePart = body.getFile("upload");
+        if (uploadFilePart != null) {
+            S3File s3File = new S3File();
+            s3File.name = uploadFilePart.getFilename();
+            s3File.file = uploadFilePart.getFile();
+            s3File.save();
+            return redirect(routes.Application.index());
+        }
+        else {
+            return badRequest("File upload error");
+        }
     }
 
 }
